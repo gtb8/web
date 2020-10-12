67d92c68d870c3d8472a211fe67db17a6c54afb3
gtb8 <guillermotb8@gmail.com>
2020-10-12 14:22:10 -0500

    Actualizacion
    2a revision

--- API_Platform/js/script.js
+++ API_Platform/js/script.js
@@ -111,5 +111,16 @@ $(function() {
     }
 
 
+    $(function() {
+        $('.selectpicker').selectpicker();
+        $('pick__lang').selectpicker();
+    });
+
+    function go__bahasa() {
+        location = document.pilih__bahasa.ipicked__bahasa.
+        options[document.pilih__bahasa.ipicked__bahasa.selectedIndex].value
+    }
+
+
 
 }); /* End Fn */
